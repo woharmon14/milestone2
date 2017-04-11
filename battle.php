fe172e7efb19794f618a4e5556e9aaf32ace7634
woharmon14 <woharmonscheiz@gmail.com>
2017-04-11 09:21:17 +0800

    Update battle.php

--- battle.php
+++ battle.php
@@ -1,201 +1,330 @@
-<?php
-session_start();
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Template</title>
-  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oxygen:400,300,700">
+	<title>Duel</title>
 
-  <link rel="stylesheet" href="css/style1.css">
-    <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
   <!-- Optional theme -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
   <!-- Latest compiled and minified JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-  <style type="text/css">
-    @font-face {
-    font-family: Pocket Monk;
-    src: url("pokemon.otf") format("opentype");
-    }
-  </style>
+	<style type="text/css">
+	* {
+		box-sizing: border-box;
 
- 	 <style>
-			body {font-family: "Lato", sans-serif;}
+	}
+	span{
+		font-weight: bold;
+		color:crimson;
+	}
+	body{
+		margin: 20px auto;
+	}
 
-			/* Style the tab */
-			div.tab {
-			    overflow: hidden;
-			    border: 1px solid #ccc;
-			    background-color: #f1f1f1;
-			}
-
-			/* Style the buttons inside the tab */
-			div.tab button {
-			    background-color: inherit;
-			    float: left;
-			    border: none;
-			    outline: none;
-			    cursor: pointer;
-			    transition: 0.3s;
-			    font-size: 17px;
-			}
-
-			/* Change background color of buttons on hover */
-			div.tab button:hover {
-			    background-color: #ddd;
-			}
-
-			/* Create an active/current tablink class */
-			div.tab button.active {
-			    background-color: #ccc;
-			}
-
-			/* Style the tab content */
-			.tabcontent {
-			    display: none;
-			    padding: 6px 12px;
-			    -webkit-animation: fadeEffect 1s;
-			    animation: fadeEffect 1s;
-			}
-
-			/* Fade in tabs */
-			@-webkit-keyframes fadeEffect {
-			    from {opacity: 0;}
-			    to {opacity: 1;}
-			}
-
-			@keyframes fadeEffect {
-			    from {opacity: 0;}
-			    to {opacity: 1;}
-			}
-	</style>
-
-</head>
-
-<body>
-<header id="head">
-	<nav class="navbar navbar-default">
-    <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar" style="width: 7%;">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>                       
-        </button>
-        <img src="http://vignette3.wikia.nocookie.net/youtubepoop/images/4/4c/Pokeball.png/revision/latest?cb=20150418234807">
-        <a class="navbar-brand" href="index.php">Poké Card Fight!</a>
-      </div>
-	    <div class="collapse navbar-collapse" id="myNavbar">
-	      <ul class="nav navbar-nav navbar-right">
-	        <li><a href="about.php"><strong>About</strong></a></li>
-	        <li><a href="pokedex.php"><strong>Pokémon</strong></a></li>
-	        <li><a href="battle.php"><strong>Battle</strong></a></li>
-	      </ul>
-	    </div>
-	  </div>
-	</nav>
-
-	<nav class="navbar navbar-default">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-9 col-md-5">
-					<h4>Search Here: </h4>
-					<input type="text" style="border-radius: 5px; border-style: solid;  border-color: #313131"></input>
-				</div>
-				<div class="col-lg-2">
-					<?php if(isset($_SESSION['username'])){
-						echo "<h5>Welcome, ".$_SESSION['username']."!</h5>";
-					} ?>
-					
-				</div>
-				<div class="col-lg-1">
-				<a href="logout.php"><img src="https://conceptdraw.com/a155c4/p137/preview/640/pict--power-button-cloud-round-icons-vector-stencils-library"></a>
-				</div>
-			</div>
-		</div>
-	</nav>
-	
-</header>
-<div style="height: 10px; background: -webkit-linear-gradient(left, rgba(248,80,50,1) 0%, rgba(241,111,92,1) 18%, rgba(240,78,56,1) 42%, rgba(240,47,23,1) 64%, rgba(246,41,12,1) 80%, rgba(240,47,23,1) 81%, rgba(240,47,24,1) 82%, rgba(231,56,39,1) 100%);"></div>
-<section>
-	<div class="container-fluid">
-		<iframe src="http://gameboy.pokemongarden.net/game.html" style="width: 100%; height: 100vh;">
-	</iframe>
-	
-
-	</div>			
-			
-	
-</section>
-<div style="height: 10px; background: -webkit-linear-gradient(left, rgba(248,80,50,1) 0%, rgba(241,111,92,1) 18%, rgba(240,78,56,1) 42%, rgba(240,47,23,1) 64%, rgba(246,41,12,1) 80%, rgba(240,47,23,1) 81%, rgba(240,47,24,1) 82%, rgba(231,56,39,1) 100%); margin-top: 20px;"></div>
-<footer>
-	<div class=" container container-fluid" style="padding: 20px; width: 100%">
-		<div class="col-lg-8">
-			<a href="#head" style="text-decoration: none; color: black;"><h5> Poké Card Fight! 2017</h5></a>
-		</div>
-		<div class="col-lg-4">
-			<img src="https://img.clipartfest.com/2ee336c8a8f6e7774fbd90dfcedecaea_15-facebook-logo-vector-art-clipart-of-facebook-logo_1200-1200.jpeg">
-			<img src="http://lcm.org/wp-content/uploads/2011/03/twitter-logo.png">
-			<img src="https://img.clipartfest.com/49803d8eb5ea235a5860ac942caece70_download-png-download-eps-instagram-logo-clipart-png_1024-1024.png">
-		</div>
+	.left, .right {
 		
-	</div>
-</footer>
+		height: 400px;
+		width: 320px;
+		margin-top: 30px;
+		
+		text-align: center;
+		background: rgba(255,255,255,0);
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	}
+	hr {
+		border: 1px solid gray;
+		margin: 0px;
+	}
+	button {
+		width: 100px;
+		background: #3897f0;
+    	border-color: #3897f0;
+    	color: #fff;
+    	border-radius: 3px;
+	    border-style: solid;
+	    border-width: 1px;
+	    font-weight: 600;
+	    outline: none;
+	    overflow: hidden;
+	    text-overflow: ellipsis;
+	    white-space: nowrap;
+	    -webkit-appearance: none;
+	    
+	    margin: 15px 50px;
+	}
+	#imgflp {
+        -moz-transform: scaleX(-1);
+        -o-transform: scaleX(-1);
+        -webkit-transform: scaleX(-1);
+        transform: scaleX(-1);
+        filter: FlipH;
+        -ms-filter: "FlipH";
+		}
+	body {
+		background: url(images/Pokémon_Stadium_2.jpg) center/cover no-repeat;
+	}
+	h4 {
+		font-weight: bold;
+	}
+	.row {
+		margin: 0;
+	}
+	.stat1, .stat2 {
+		border: 2px solid blue;
+		background-color: rgba(255,255,255,0.8);
+		width: 18%;
+		height: 190px;
+		border-radius: 10%;
 
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	}
 
-<script>
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+</style>
+</head>
+<body>
+<div >
+	<div style="text-align: center;"><img src="images/name.png" style="width: 300px; height: 100px;"></div>
+	
+<?php
+class Pokemon {
+	protected $name,$type,$hp,$ap,$def,$img;
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+	function __construct($name,$type,$hp,$ap,$def,$img) {
 
-      // Store hash
-      var hash = this.hash;
+		$this->name = $name;
+		$this->type = $type;
+		$this->hp = $hp;
+		$this->ap = $ap;
+		$this->def = $def;
+		$this->img = $img;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 3800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-});
-</script>
-<script>
-function openCity(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
+	}
+
+	function set_name ($name){
+		$this->name = $name;
+	}
+
+	function get_name (){
+		return "<p>"."<span>Trainer: </span>".$this->name."</p>";
+	}
+
+	function set_type ($type){
+		$this->type = $type;
+	}
+
+	function get_type (){
+		return "<p>"."<span>Element: </span>".$this->type ."</p>";
+	}
+
+	function set_hp ($hp){
+		$this->hp = $hp;
+	}
+
+	function get_hp (){
+		return $this->hp;
+	}
+	function set_ap ($ap){
+		$this->ap = $ap;
+	}
+
+	function get_ap (){
+		return $this->ap;
+	}
+	function set_def ($def){
+		$this->def = $def;
+	} 	
+
+	function get_def (){
+		return $this->def;
+	}
+
+	function set_img ($img){
+		$this->img = $img;
+	} 	
+
+	function get_img (){
+		return "<p>".$this->img ."</p>";
+	}
 }
-document.getElementById("defaultOpen").click();
+
+
+
+
+class Fire extends Pokemon
+{
+	
+	function __construct($name) {
+		$images = ["<img src='http://www.pokestadium.com/sprites/xy/moltres.gif'>","<img src='http://www.pokestadium.com/sprites/xy/entei.gif'>","<img src='http://www.pokestadium.com/sprites/xy/groudon.gif'>","<img src='http://www.pokestadium.com/sprites/xy/charizard-3.gif'>","<img src='http://www.pokestadium.com/sprites/xy/ho-oh.gif'>","<img src='http://www.pokestadium.com/sprites/xy/flareon-3.gif'>"];
+
+		$this->name = $name;
+		$this->type = "fire";
+		$this->hp = rand(100,200);
+		$this->ap = rand(40,60);
+		$this->def = rand(5,20);
+		$this->img = $images[rand(0,count($images)-1)];
+	}
+}
+
+class Ice extends Pokemon
+{
+	
+	function __construct($name) {
+		$images = ["<img src='http://www.pokestadium.com/sprites/xy/articuno.gif'>","<img src='http://www.pokestadium.com/sprites/xy/suicune.gif'>","<img src='http://www.pokestadium.com/sprites/xy/kyogre.gif'>","<img src='http://www.pokestadium.com/sprites/xy/blastoise-3.gif'>","<img src='http://www.pokestadium.com/sprites/xy/kyurem.gif'>","<img src='http://www.pokestadium.com/sprites/xy/lugia.gif'>","<img src='http://www.pokestadium.com/sprites/xy/gyarados-2.gif'>","<img src='http://www.pokestadium.com/sprites/xy/vaporeon-3.gif'>","<img src='http://www.pokestadium.com/sprites/xy/dragonite-3.gif'>"];
+
+		$this->name = $name;
+		$this->type = "Ice";
+		$this->hp = rand(100,200);
+		$this->ap = rand(20,40);
+		$this->def = rand(20,40);
+		$this->img = $images[rand(0,count($images)-1)];
+
+	}
+}
+
+class Lightning extends Pokemon
+{
+	
+	function __construct($name) {
+		$images = ["<img src='http://www.pokestadium.com/sprites/xy/zapdos.gif'>","<img src='http://www.pokestadium.com/sprites/xy/raikou.gif'>","<img src='http://www.pokestadium.com/sprites/xy/rayquaza.gif'>","<img src='http://www.pokestadium.com/sprites/xy/zekrom.gif'>","<img src='http://www.pokestadium.com/sprites/xy/pikachu-5.gif'>"];
+
+		$this->name = $name;
+		$this->type = "Lightning";
+		$this->hp = rand(100,200);
+		$this->ap = rand(10,70);
+		$this->def = rand(0,50);
+		$this->img = $images[rand(0,count($images)-1)];
+
+	}
+}
+?>
+	<div class="row" style="margin-left: 150px;">
+	<div class="col-lg-3 stat1">
+		<div style="text-align: center;">
+				<?php 
+					$name = ['Ann','Ben','Cory','Dennis','Elvis'];
+					$type = ['Fire','Ice','Lightning'];
+					shuffle($name);
+					for ($i=0; $i <1; $i++) { 
+					$elem = rand(0, count($type)-1);
+					$class = $type[$elem];
+					$char = new $class($name[$i]);
+
+					echo "<br>";
+					echo $char -> get_name();
+					echo $char -> get_type();
+					echo "<p>"."<span>Health Points: </span>"."<span id='t1hp'>".$char -> get_hp()."</span></p>";
+					echo "<p>"."<span>Attack Points: </span>"."<span id='t1ap'>".$char -> get_ap()."</span></p>";
+					echo "<p>"."<span>Defense: </span>"."<span id='t1def'>".$char -> get_def()."</span></p>";
+					
+						}
+
+				?>
+			</div>
+	</div>
+		<div class="col-lg-3 left" style="position: relative;">
+			
+			<div style="position: absolute; top: 53%; left: 50%; transform: translate(-50%, -50%);">
+				<?php 
+					
+
+					echo "<div id='imgflp'>";
+					echo $char -> get_img() . "</div>";
+					
+					
+						
+
+				?>
+			</div>
+			
+		</div>
+		<div class="col-lg-3 right" style="position: relative;">
+			
+			<div style="position: absolute; top: 53%; left: 50%; transform: translate(-50%, -50%);">
+				<?php 
+					$name = ['Ann','Ben','Cory','Dennis','Elvis'];
+					$type = ['Fire','Ice','Lightning'];
+					shuffle($name);
+					for ($i=0; $i <1; $i++) { 
+					$elem = rand(0, count($type)-1);
+					$class = $type[$elem];
+					$char = new $class($name[$i]);
+
+					echo $char -> get_img();
+					?>				
+					
+
+			</div>
+			
+		</div>
+		<div class="col-lg-3 stat2" style="text-align: center;">
+		<?php
+					echo "<br>";
+					echo $char -> get_name();
+					echo $char -> get_type();
+					echo "<p>"."<span>Health Points: </span>"."<span id='t2hp'>".$char -> get_hp()."</span></p>";
+					echo "<p>"."<span>Attack Points: </span>"."<span id='t2ap'>".$char -> get_ap()."</span></p>";
+					echo "<p>"."<span>Defense: </span>"."<span id='t2def'>".$char -> get_def()."</span></p>";
+
+						}
+
+				?>
+					
+		</div>
+	</div>
+<div style="text-align: center; vertical-align: middle;">
+<br> 
+	<button onclick="myFunction()">Roll 'em Up</button>
+	<button onclick="fight()"> Duel!</button>
+
+</div>
+
+</div>
+
+<script type="text/javascript">
+	function fight(){
+		var t1hp = $('#t1hp').html();
+		var t1ap = $('#t1ap').html();
+		var t1def = $('#t1def').html();
+		var t2hp = $('#t2hp').html();
+		var t2ap = $('#t2ap').html();
+		var t2def = $('#t2def').html();
+
+		$.post("fight_api.php",
+	    {
+	        t1hp: t1hp,
+	        t1def: t1def,
+	        t2ap: t2ap,
+	    },
+	    function(data, status){
+	    	if(data<=0){
+	    		alert("Congratulations, Team2 wins!");
+	    	}
+	        $('#t1hp').html(data);
+	    });
+
+	    $.post("fight_api.php",
+	    {
+	        t2hp: t2hp,
+	        t2def: t2def,
+	        t1ap: t1ap,
+	    },
+	    function(data, status){
+	    	if(data<=0){
+	    		alert("Congratulations, Team1 wins!");
+	    	}
+	        $('#t2hp').html(data);
+	    });
+	}
 </script>
 
 </body>
+<script>
+function myFunction() {
+    location.reload();
+}
+</script>
 </html>
